a54e5b2df5265dc913348231f1d3ea49e06d3a42
Grant <Grant@THINKPAD>
2019-11-06 09:42:47 +0200

    Added new addons, improved addon ecosystem, changed file upload from base64 to file system

---
+++ RazorSharp/App_Code/ShortCodes.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Text.RegularExpressions;
+
+/// <summary>
+/// Class to replace shortcodes that look like [ShortCode/] with partials from Addons section
+/// </summary>
+
+public class ShortCodes
+{
+    public static string RepaceToken(string data)
+    {
+        string token = @"\[(.*?)\/\]";
+        Regex objRegex = new Regex(token);
+        MatchCollection objCol = objRegex.Matches(data);
+        foreach (Match item in objCol)
+        {
+            data = data.Replace(item.Groups[0].Value, BuildControl(item.Groups[1].Value.ToString()));
+        }
+        return data;
+    }
+
+    public static string BuildControl(string paramString)
+    {
+        string[] paramArr = paramString.Split(' ');
+        ArrayList arrList = new ArrayList();
+        foreach (string a in paramArr)
+        {
+            arrList.Add(a);
+        }
+        if (arrList.Count < 2) arrList.Add("");
+        string controlPath = "~/Addons/" + arrList[0] + "/Body/" + arrList[0];
+        return controlPath;
+    }
+}
